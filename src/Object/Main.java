2440ec097c2e1151cacfc8adfc41946827a397d2
Daniel Sangalang <danielsangalang.21@gmail.com>
2020-09-03 17:55:35 +0800

    I'm trying it

---
+++ src/Object/Main.java
@@ -0,0 +1,15 @@
+package Object;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Point originOne = new Point(23,90);
+		
+		Rectangle rectOne = new Rectangle(originOne);
+		System.out.println(rectOne.origin.x);
+int h = new Rectangle().height;
+System.out.println(h);
+	}
+
+}
